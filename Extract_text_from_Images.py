60920fa26a3bf0504e6110bf6514e44eb67f8194
dasarisasidhar <dasarisasidhar.d@gmail.com>
2020-02-10 18:05:03 +0530

    Update Extract_text_from_Images.py

--- Extract_text_from_Images.py
+++ Extract_text_from_Images.py
@@ -2,7 +2,7 @@ from PIL import Image
 import pytesseract
 
 # Download tesseract from https://github.com/UB-Mannheim/tesseract/wiki and install it.
-# insted of my path copy your path where the Tesseract is installed and tesseract.exe exist
+# Give the Tesseract installed path where tesseract.exe is exist
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Users\Sasi\AppData\Local\Tesseract-OCR\tesseract.exe"
  
